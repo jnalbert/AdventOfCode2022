26d8d29f1ad165104c139b07ea1438a90d48e4f1
Justin Albert <Jnalbert879@gmail.com>
2022-12-01 10:10:15 -0800

    day one puzzle in progress

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,8 +1,16 @@
 import Puzzle from '../../types/AbstractPuzzle';
 
 export default class ConcretePuzzle extends Puzzle {
-  public solveFirst(): string {
-    return 'day 1 solution 1';
+  public solveFirst(): string { 
+    let maxCalories = 0;
+
+    // split the string at the empty line
+    const elves = this.input.split('\n\n');
+    
+    
+    console.log(elves)
+
+    return "blank"
   }
   public solveSecond(): string {
     return 'day 1 solution 2';
