42beb865363d418865f6aec06068931bbd106c84
Justin Albert <65417105+jnalbert@users.noreply.github.com>
2022-12-02 00:02:28 +0000

    finished day one challenge

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,25 +1,47 @@
+/* eslint-disable semi */
 import Puzzle from '../../types/AbstractPuzzle';
 
 export default class ConcretePuzzle extends Puzzle {
   public solveFirst(): string { 
-    let maxCalories = 0;
+    let maxCalories = 0
 
     // split the string at the empty line
-    const elves = this.input.split('\n\n');
+    const elves = this.input.split('\n\n').map((elf) => elf.split('\n'))
     
-    
-    console.log(elves)
+    elves.forEach((elf) => {
+        let calories = 0;
+        elf.forEach(food => calories += parseInt(food))
+        maxCalories = Math.max(maxCalories, calories)
+    })
 
-    return "blank"
+    return maxCalories.toString()
   }
   public solveSecond(): string {
-    return 'day 1 solution 2';
-  }
+
+    const maxCalorieElves: number[] = []
+
+    // splits the string into individual elves
+    const elves = this.input.split('\n\n').map((elf) => elf.split('\n'))
+
+    elves.forEach((elf) => {
+        let calories = 0;
+        elf.forEach(food => calories += parseInt(food))
+
+        maxCalorieElves.push(calories)
+        // sort the calories from greatest to least
+        maxCalorieElves.sort((a, b) => b - a)
+        // if the array is greater than 3, remove the last element
+        maxCalorieElves.length > 3 && maxCalorieElves.pop()
+    })
+
+    // return the sum of the maxCaloriesElves array
+    return maxCalorieElves.reduce((a, b) => a + b).toString()
+}
 
   public getFirstExpectedResult(): string {
-    return 'day 1 solution 1';
+    return '70720';
   }
   public getSecondExpectedResult(): string {
-    return 'day 1 solution 2';
+    return '207148';
   }
 }
