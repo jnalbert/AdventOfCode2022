e60d140f620b8c28662249781ca0cf3160ac1f55
Justin Albert <Jnalbert879@gmail.com>
2022-12-13 10:59:49 -0800

    day 10 not done

--- src/days/10/Puzzle.ts
+++ src/days/10/Puzzle.ts
@@ -7,7 +7,7 @@ interface instruction {
 
 export default class ConcretePuzzle extends Puzzle {
 
-    private getInstructions(): any[] {
+    private getInstructions(): instruction[] {
         return this.input.split('\n').map((line) => {
             const commands = line.split(' ');
             if (commands[1]) {
@@ -20,19 +20,31 @@ export default class ConcretePuzzle extends Puzzle {
   public solveFirst(): string {
     const instructions = this.getInstructions();
     let register = 1;
-    let cycle = 0;
+    let cycle = 1;
+    let signalStrength = 0;
+    let canContinueThrough = false;
 
 
     for (let i = 0; i < instructions.length; i++) {
         const instruction = instructions[i].command;
 
-        if (instruction === 'addx') {
-            register += instructions[i].value;
+        if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
+            signalStrength += register * cycle;
         }
+        
+        if (instruction === 'addx') {
+            if (canContinueThrough) {
+                register += Number(instructions[i].value)
+            } else {
+                i--;
+            }
+            canContinueThrough = !canContinueThrough;
+        }
+        cycle++
 
-        console.log(instruction);
+     
     }
-    return 'day 1 solution 1';
+    return signalStrength.toString();
   }
 
   public getFirstExpectedResult(): string {
@@ -41,8 +53,47 @@ export default class ConcretePuzzle extends Puzzle {
   }
 
   public solveSecond(): string {
-    // WRITE SOLUTION FOR TEST 2
-    return 'day 1 solution 2';
+    const instructions = this.getInstructions();
+    let register = 1;
+    let cycle = 1;
+    let canContinueThrough = false;
+    let indexCRT = 0
+    const CRT: string[] = ["", "", "", "", "", ""];
+
+
+    for (let i = 0; i < instructions.length; i++) {
+        const instruction = instructions[i].command;
+
+        console.log(`cycle: ${cycle} i: ${i} instruction: ${instruction} register: ${register} canContinueThrough: ${canContinueThrough}`);
+
+         // check if the cycle is within one of the register
+         if (Math.abs(register - cycle) <= 1) {
+            CRT[indexCRT] += "#";
+        } else {
+            CRT[indexCRT] += ".";
+        }
+
+        if (cycle % 40 === 0) {
+            indexCRT++;
+            cycle -= 39;
+        }
+      
+        if (instruction === 'addx') {
+            if (canContinueThrough) {
+                register += Number(instructions[i].value)
+            } else {
+                i--;
+            }
+            canContinueThrough = !canContinueThrough;
+        }
+
+        cycle++
+
+
+     
+    }
+    return CRT.toString();
+
   }
 
   public getSecondExpectedResult(): string {
